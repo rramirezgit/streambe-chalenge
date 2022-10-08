8f2a8d12f8fef9843a79b77fa5a1bdae366552a2
ricardo.ramirez <rramirez@tupaca.com>
2022-10-08 18:15:07 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint-config-prettier',
+    'plugin:react/recommended',
+    'react-app',
+    'standard-with-typescript',
+    'plugin:react/jsx-runtime'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['react'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+      '@typescript-eslint/explicit-function-return-type': 'warn'
+    }
+  }
+}
